1c72bff103710156e0e31253116aa3f1c3125381
Maxence PAPILLON <maxence.papillon@outlook.com>
2017-06-21 14:30:36 +0200

    Update headlines page tests

--- nouvelles/tests/test_article_news.py
+++ nouvelles/tests/test_article_news.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 
+from django.db.models import QuerySet
 from django.test import TestCase
 from django.urls import reverse
 from freezegun import freeze_time
@@ -12,6 +13,6 @@ class ArticleNewsTestCase(TestCase):
 
     def test_headlines_success(self):
         response = self.client.get(reverse('index'))
-        self.failUnless(isinstance(response.context['articles'], OrderedDict))
+        self.failUnless(isinstance(response.context['article_list'], QuerySet))
         self.assertTemplateUsed(response, "nouvelles/article_news_list.html")
         self.failUnlessEqual(response.status_code, 200)
